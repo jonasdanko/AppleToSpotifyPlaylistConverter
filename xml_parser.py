f68debba63cfd1940ef48730f9d8496669e593f2
[jonasdanko] <[jonas12danko@gmail.com]>
2020-04-21 12:20:13 -0400

    Fixed bug with ' and & in songs

--- xml_parser.py
+++ xml_parser.py
@@ -1,6 +1,5 @@
 import xml.etree.ElementTree as ET
 
-
 def parse_xml(filename):
     tree = ET.parse(filename)
     if tree:
@@ -15,18 +14,35 @@ def get_songlist(tree):
         name=d.find('string[1]')
         artist=d.find('string[2]')
         song_name = ""
-        if name == None and artist==None:
+        artist_name = ""
+        if name == None or artist == None:
             pass
         else:
             if name.text.find("(") != -1:
                 index = name.text.find("(")
                 song_name = name.text[:index-1]
                 song_name = song_name.replace("'","")
-                key = [song_name, artist.text]
-            else:
+                song_name = song_name.replace("&", "")
+                artist_name = artist.text
+                artist_name = artist_name.replace("'","")
+                artist_name = artist_name.replace("&","")
+                artist_name = artist_name.replace(",","")
+                key = [song_name, artist_name]
+            else:  
                 song_name = name.text.replace("'","")
-                key = [song_name, artist.text]
+                song_name = song_name.replace("&", "")
+                artist_name = artist.text
+                if artist_name != None:
+                    artist_name = artist_name.replace("'","")
+                    artist_name = artist_name.replace("&","")
+                    artist_name = artist_name.replace(",","")
+                key = [song_name, artist_name]
             songs.append(key)
     songs.pop(0)
     songs.pop(len(songs)-1)
     return songs
+
+
+#tree = parse_xml('rocktheboat.xml')
+#songs = get_songlist(tree)
+#print(songs)
